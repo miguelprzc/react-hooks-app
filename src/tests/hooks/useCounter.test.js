99cf407bfb4dcc1f92416c7118d7435e18a6eac5
Miguel Perez Contreras <mapcmiguel28@gmail.com>
2022-02-04 14:41:27 -0400

    Implement tests to useCounter functions

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "../../hooks/useCounter";
 
 describe('Test in useCounter', () => {
@@ -10,4 +10,51 @@ describe('Test in useCounter', () => {
     expect(typeof result.current.decrement).toBe('function');
     expect(typeof result.current.reset).toBe('function');
   });
+
+  test('should return counter in 105', () => {
+    const {result} = renderHook(() => useCounter(105));
+
+    expect(result.current.counter).toBe(105);
+  });
+
+  test('should increment counter by 1', () => {
+    const {result} = renderHook(() => useCounter(105));
+    const {increment} = result.current;
+
+    act(() => {
+      increment();
+    });
+
+    const {counter} = result.current;
+
+    expect(counter).toBe(106);
+  });
+
+  test('should decrement counter by 1', () => {
+    const {result} = renderHook(() => useCounter());
+    const {decrement} = result.current;
+    
+    act(() => {
+      decrement();
+    });
+    
+    const {counter} = result.current;
+    expect(counter).toBe(9);
+  });
+
+  test('should reset counter ', () => {
+    const {result} = renderHook(() => useCounter());
+    const {decrement, reset} = result.current;
+    
+    act(() => {
+      decrement();
+      reset();
+    });
+    
+    const {counter} = result.current;
+    expect(counter).toBe(10);
+  });
+  
+  
+
 });
